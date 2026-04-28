a78012d6b064effd3154fb61403b8cc989177478
NEXY <goif74945@gmail.com>
2026-04-28 21:43:11 +0700

    Update prisma-client.ts

--- tests/stubs/prisma-client.ts
+++ tests/stubs/prisma-client.ts
@@ -1,1 +1,25 @@
+export type PrismaClientStubState = {
+  connected: boolean;
+};
 
+export class PrismaClient {
+  readonly state: PrismaClientStubState;
+
+  constructor() {
+    this.state = {
+      connected: false,
+    };
+  }
+
+  async $connect(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  async $disconnect(): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
+export function createPrismaClientStub(): PrismaClient {
+  return new PrismaClient();
+}
